1a56656aafa04072c7bb24bf7af5da89f8f28aba
hekang1992 <hekang19921111@hotmail.com>
2026-03-25 13:07:31 +0800

    app

--- PinjamCepat/Home/View/EnView.swift
+++ PinjamCepat/Home/View/EnView.swift
@@ -10,14 +10,13 @@ import SnapKit
 
 class EnView: BaseView {
     
+    // MARK: - Model
     var model: yieldedModel? {
-        didSet {
-            guard let model = model else { return }
-            headView.model = model
-        }
+        didSet { updateUI() }
     }
     
-    lazy var scrollView: UIScrollView = {
+    // MARK: - UI Components
+    private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.showsVerticalScrollIndicator = false
@@ -25,63 +24,95 @@ class EnView: BaseView {
         return scrollView
     }()
     
-    lazy var contentView: UIView = {
+    private lazy var contentView: UIView = {
         let contentView = UIView()
         return contentView
     }()
     
-    lazy var headView: EndCardView = {
+    private lazy var headView: EndCardView = {
         let headView = EndCardView(frame: .zero)
         return headView
     }()
     
-    lazy var oneImageView: UIImageView = {
-        let oneImageView = UIImageView()
-        oneImageView.image = UIImage(named: "en_ys_image".localized)
-        oneImageView.contentMode = .center
-        return oneImageView
+    private lazy var oneImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "en_ys_image".localized)
+        imageView.contentMode = .center
+        return imageView
     }()
     
-    lazy var twoImageView: UIImageView = {
-        let twoImageView = UIImageView()
-        twoImageView.image = UIImage(named: "en_bz_image".localized)
-        twoImageView.contentMode = .center
-        return twoImageView
+    private lazy var twoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "en_bz_image".localized)
+        imageView.contentMode = .center
+        return imageView
     }()
     
+    // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
+        setupConstraints()
+    }
+    
+    @MainActor required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Private Methods
+private extension EnView {
+    
+    func setupUI() {
         addSubview(scrollView)
         scrollView.addSubview(contentView)
         contentView.addSubview(headView)
-        
+        contentView.addSubview(oneImageView)
+        contentView.addSubview(twoImageView)
+    }
+    
+    func setupConstraints() {
+        setupScrollViewConstraints()
+        setupContentViewConstraints()
+        setupHeadViewConstraints()
+        setupOneImageViewConstraints()
+        setupTwoImageViewConstraints()
+    }
+    
+    func setupScrollViewConstraints() {
         scrollView.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide)
             make.left.right.bottom.equalToSuperview()
         }
-        
+    }
+    
+    func setupContentViewConstraints() {
         contentView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
             make.width.equalTo(scrollView.snp.width)
         }
+    }
+    
+    func setupHeadViewConstraints() {
+        let isEnglish = LanguageManager.shared.getCurrentLanguage() == .english
+        let height: CGFloat = isEnglish ? 292.pix() : 270.pix()
         
         headView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(18)
             make.centerX.equalToSuperview()
-            if LanguageManager.shared.getCurrentLanguage() == .english {
-                make.size.equalTo(CGSize(width: 375.pix(), height: 292.pix()))
-            }else {
-                make.size.equalTo(CGSize(width: 375.pix(), height: 270.pix()))
-            }
+            make.size.equalTo(CGSize(width: 375.pix(), height: height))
         }
-        
-        contentView.addSubview(oneImageView)
-        contentView.addSubview(twoImageView)
+    }
+    
+    func setupOneImageViewConstraints() {
         oneImageView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(headView.snp.bottom).offset(22)
             make.size.equalTo(CGSize(width: 335.pix(), height: 268.pix()))
         }
+    }
+    
+    func setupTwoImageViewConstraints() {
         twoImageView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(oneImageView.snp.bottom).offset(20)
@@ -89,9 +120,9 @@ class EnView: BaseView {
             make.bottom.equalToSuperview().offset(-20.pix())
         }
     }
-     
-    @MainActor required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
+    func updateUI() {
+        guard let model = model else { return }
+        headView.model = model
+    }
 }
--- PinjamCepat/Home/View/EndCardView.swift
+++ PinjamCepat/Home/View/EndCardView.swift
@@ -11,171 +11,213 @@ import Kingfisher
 
 class EndCardView: BaseView {
     
+    // MARK: - Model
     var model: yieldedModel? {
-        didSet {
-            guard let model = model else { return }
-            let logoUrl = model.trouble ?? ""
-            logoImageView.kf.setImage(with: URL(string: logoUrl))
-            nameLabel.text = model.brain ?? ""
-            oneLabel.text = model.poorly ?? ""
-            twoLabel.text = model.beseech ?? ""
-            lBtn.setTitle("  " + (model.skill ?? ""), for: .normal)
-            rBtn.setTitle("  " + (model.verily ?? ""), for: .normal)
-            applyBtn.setTitle(model.sabbath ?? "", for: .normal)
-        }
+        didSet { updateUI() }
     }
     
-    lazy var headImageView: UIImageView = {
-        let headImageView = UIImageView()
-        headImageView.image = UIImage(named: "enh_head_image".localized)
-        return headImageView
+    // MARK: - UI Components
+    private lazy var headImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "enh_head_image".localized)
+        return imageView
     }()
     
-    lazy var logoImageView: UIImageView = {
-        let logoImageView = UIImageView()
-        logoImageView.layer.cornerRadius = 5
-        logoImageView.layer.masksToBounds = true
-        return logoImageView
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 5
+        imageView.layer.masksToBounds = true
+        return imageView
     }()
     
-    lazy var nameLabel: UILabel = {
-        let nameLabel = UILabel()
-        nameLabel.textAlignment = .left
-        nameLabel.textColor = .white
-        nameLabel.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
-        return nameLabel
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        return label
     }()
     
-    lazy var oneLabel: UILabel = {
-        let oneLabel = UILabel()
-        oneLabel.textAlignment = .left
-        oneLabel.textColor = UIColor.init(hexString: "#7E7E7E")
-        oneLabel.font = UIFont.systemFont(ofSize: 12, weight: .medium)
-        return oneLabel
+    private lazy var oneLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = UIColor(hexString: "#7E7E7E")
+        label.font = .systemFont(ofSize: 12, weight: .medium)
+        return label
     }()
     
-    lazy var twoLabel: UILabel = {
-        let twoLabel = UILabel()
-        twoLabel.textAlignment = .left
-        twoLabel.textColor = UIColor.init(hexString: "#262626")
-        twoLabel.font = UIFont.systemFont(ofSize: 48, weight: .bold)
-        return twoLabel
+    private lazy var twoLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = UIColor(hexString: "#262626")
+        label.font = .systemFont(ofSize: 48, weight: .bold)
+        return label
     }()
     
-    lazy var lBtn: UIButton = {
-        let lBtn = UIButton(type: .custom)
-        lBtn.isEnabled = false
-        lBtn.setTitleColor(UIColor.init(hexString: "#494949"), for: .normal)
-        lBtn.titleLabel?.font = UIFont.systemFont(ofSize: 13, weight: .medium)
-        lBtn.setImage(UIImage(named: "lf_icon_image"), for: .normal)
-        return lBtn
+    private lazy var leftButton: UIButton = {
+        return createButton(imageName: "lf_icon_image")
     }()
     
-    lazy var rBtn: UIButton = {
-        let rBtn = UIButton(type: .custom)
-        rBtn.isEnabled = false
-        rBtn.setTitleColor(UIColor.init(hexString: "#494949"), for: .normal)
-        rBtn.titleLabel?.font = UIFont.systemFont(ofSize: 13, weight: .medium)
-        rBtn.setImage(UIImage(named: "rf_icon_image"), for: .normal)
-        return rBtn
+    private lazy var rightButton: UIButton = {
+        return createButton(imageName: "rf_icon_image")
     }()
     
-    lazy var policyBtn: UIButton = {
-        let policyBtn = UIButton(type: .custom)
-        policyBtn.adjustsImageWhenHighlighted = false
-        policyBtn.setBackgroundImage(UIImage(named: "hc_loan_image"), for: .normal)
-        return policyBtn
+    private lazy var policyButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.adjustsImageWhenHighlighted = false
+        button.setBackgroundImage(UIImage(named: "hc_loan_image"), for: .normal)
+        return button
     }()
     
-    lazy var applyBtn: UIButton = {
-        let applyBtn = UIButton(type: .custom)
-        applyBtn.setTitleColor(.white, for: .normal)
-        applyBtn.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        applyBtn.setBackgroundImage(UIImage(named: "app_btn_bg_image"), for: .normal)
-        return applyBtn
+    private lazy var applyButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.setBackgroundImage(UIImage(named: "app_btn_bg_image"), for: .normal)
+        return button
     }()
     
+    // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
+        setupConstraints()
+    }
+    
+    @MainActor required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Private Methods
+private extension EndCardView {
+    
+    func createButton(imageName: String) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.isEnabled = false
+        button.setTitleColor(UIColor(hexString: "#494949"), for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 13, weight: .medium)
+        button.setImage(UIImage(named: imageName), for: .normal)
+        return button
+    }
+    
+    func setupUI() {
         addSubview(headImageView)
         headImageView.addSubview(logoImageView)
         headImageView.addSubview(nameLabel)
         headImageView.addSubview(oneLabel)
         headImageView.addSubview(twoLabel)
-        headImageView.addSubview(lBtn)
-        headImageView.addSubview(rBtn)
+        headImageView.addSubview(leftButton)
+        headImageView.addSubview(rightButton)
         
+        let isEnglish = LanguageManager.shared.getCurrentLanguage() == .english
+        
+        if isEnglish {
+            addSubview(policyButton)
+            addSubview(applyButton)
+        } else {
+            addSubview(applyButton)
+        }
+    }
+    
+    func setupConstraints() {
+        setupHeadImageViewConstraints()
+        setupLogoImageViewConstraints()
+        setupNameLabelConstraints()
+        setupOneLabelConstraints()
+        setupTwoLabelConstraints()
+        setupButtonsConstraints()
+        setupBottomButtonsConstraints()
+    }
+    
+    func setupHeadImageViewConstraints() {
         headImageView.snp.makeConstraints { make in
             make.top.equalToSuperview()
             make.left.equalToSuperview().offset(20.pix())
             make.size.equalTo(CGSize(width: 355.pix(), height: 192.pix()))
         }
-        
+    }
+    
+    func setupLogoImageViewConstraints() {
         logoImageView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(7.pix())
             make.left.equalToSuperview().offset(19.pix())
             make.width.height.equalTo(28.pix())
         }
-        
+    }
+    
+    func setupNameLabelConstraints() {
         nameLabel.snp.makeConstraints { make in
             make.centerY.equalTo(logoImageView)
             make.left.equalTo(logoImageView.snp.right).offset(6)
             make.height.equalTo(20)
         }
-        
+    }
+    
+    func setupOneLabelConstraints() {
         oneLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(55.pix())
             make.left.equalToSuperview().offset(19.pix())
             make.height.equalTo(15)
         }
-        
+    }
+    
+    func setupTwoLabelConstraints() {
         twoLabel.snp.makeConstraints { make in
             make.top.equalTo(oneLabel.snp.bottom)
             make.left.equalToSuperview().offset(19.pix())
             make.height.equalTo(59)
         }
-        
-        lBtn.snp.makeConstraints { make in
+    }
+    
+    func setupButtonsConstraints() {
+        leftButton.snp.makeConstraints { make in
             make.left.equalToSuperview().offset(19.pix())
             make.height.equalTo(22)
             make.top.equalTo(twoLabel.snp.bottom).offset(10)
         }
         
-        rBtn.snp.makeConstraints { make in
-            make.left.equalTo(lBtn.snp.right).offset(23)
+        rightButton.snp.makeConstraints { make in
+            make.left.equalTo(leftButton.snp.right).offset(23)
             make.height.equalTo(22)
             make.top.equalTo(twoLabel.snp.bottom).offset(10)
         }
+    }
+    
+    func setupBottomButtonsConstraints() {
+        let isEnglish = LanguageManager.shared.getCurrentLanguage() == .english
+        let topOffset = isEnglish ? 13.pix() : 20.pix()
         
-        if LanguageManager.shared.getCurrentLanguage() == .english {
-            addSubview(policyBtn)
-            addSubview(applyBtn)
-            
-            policyBtn.snp.makeConstraints { make in
+        if isEnglish {
+            policyButton.snp.makeConstraints { make in
                 make.centerX.equalToSuperview()
-                make.top.equalTo(headImageView.snp.bottom).offset(13.pix())
+                make.top.equalTo(headImageView.snp.bottom).offset(topOffset)
                 make.size.equalTo(CGSize(width: 278.pix(), height: 16.pix()))
             }
             
-            applyBtn.snp.makeConstraints { make in
+            applyButton.snp.makeConstraints { make in
                 make.centerX.equalToSuperview()
-                make.top.equalTo(policyBtn.snp.bottom).offset(11.pix())
+                make.top.equalTo(policyButton.snp.bottom).offset(11.pix())
                 make.size.equalTo(CGSize(width: 293.pix(), height: 58.pix()))
             }
-            
-        }else {
-            addSubview(applyBtn)
-            
-            applyBtn.snp.makeConstraints { make in
+        } else {
+            applyButton.snp.makeConstraints { make in
                 make.centerX.equalToSuperview()
-                make.top.equalTo(headImageView.snp.bottom).offset(20.pix())
+                make.top.equalTo(headImageView.snp.bottom).offset(topOffset)
                 make.size.equalTo(CGSize(width: 293.pix(), height: 58.pix()))
             }
         }
-        
     }
     
-    @MainActor required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    func updateUI() {
+        guard let model = model else { return }
+        
+        logoImageView.kf.setImage(with: URL(string: model.trouble ?? ""))
+        nameLabel.text = model.brain
+        oneLabel.text = model.poorly
+        twoLabel.text = model.beseech
+        leftButton.setTitle("  " + (model.skill ?? ""), for: .normal)
+        rightButton.setTitle("  " + (model.verily ?? ""), for: .normal)
+        applyButton.setTitle(model.sabbath, for: .normal)
     }
 }
